54708fdb2009b679437db6a02f43f9aef5c887e7
VivienLN <vivienleneez@gmail.com>
2023-05-22 18:10:01 +0200

    content (thank you chatGPT)

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,10 +1,10 @@
 export default [
   {
     slug: "adidas-decathlon",
-    title: "Adidas & Decathlon",
-    subtitle: "Store pages",
+    title: "Adidas Eco-conception",
+    subtitle: "Pages Decathlon",
     thumbnail: "adidas/thumb3d.png",
-    banner: "adidas/banner.jpg",
+    banner: "adidas/banner2.jpg",
     info: [
       {
         title: "Roles",
@@ -12,7 +12,7 @@ export default [
       },
       {
         title: "Technos",
-        items: ["Bootstrap 4", "CSS (Parallax 3D)"]
+        items: ["Bootstrap 4", "CSS - module 3D", "JS Vanilla"]
       },
       {
         title: "Equipe",
@@ -20,7 +20,7 @@ export default [
       },
       {
         title: "Client",
-        items: ["Adidas"]
+        items: ["Adidas", "Decathlon"]
       },
       {
         title: "Annee",
@@ -30,17 +30,25 @@ export default [
     content: [
       {
         type: "text",
-        content: "En 2020, Adidas lance deux gammes de produits en matières recyclées."
+        content: "Depuis les années 1990, Adidas a progressivement renforcé son engagement envers l'éco-conception, lançant des gammes de produits durables, notamment la collection \"Parley for the Oceans\" en 2015, utilisant des matériaux recyclés provenant de déchets plastiques océaniques."
       },
       {
         type: "text",
-        content: "Nous avons développé une page boutique dédiée sur Décathlon, ainsi qu'une page de présentation du procédé de recyclage et de fabrication de ces produits. Elles sont traduites en cinq langues."
+        content: "En 2019, adidas a lancé les collections \"Primeblue\" et \"Primegreen\", qui intègrent des matériaux recyclés à base de plastique marin ou recyclés sans utiliser de plastique vierge."
       },
       {
         type: "video",
         mime: "video/mp4",
         url: "adidas/adidas-scroll.mp4",
-        variant: "large",
+        variant: "full",
+      },
+      {
+        type: "text",
+        content: "Avec l'agence R2, j'ai intégré une page de présentation de ce procédé de recyclage et de fabrication, traduite en cinq langues."
+      },
+      {
+        type: "text",
+        content: "J'ai également intégré un template de page de catalogue dédiée, en collaboration avec l'équipe de Décathlon."
       },
       {
         type: "image",
@@ -52,8 +60,52 @@ export default [
   {
     slug: "jam-gg",
     title: "Jam.gg",
-    subtitle: "Retrogaming website",
-    thumbnail: "jamgg/thumb3d.png"
+    subtitle: "Retrogaming",
+    thumbnail: "jamgg/thumb3d.png",
+    banner: "jamgg/banner.jpg",
+    info: [
+      {
+        title: "Roles",
+        items: ["Développement front-end"]
+      },
+      {
+        title: "Technos",
+        items: ["GSAP", "Boostrap 4", "JS Vanilla"]
+      },
+      {
+        title: "Equipe",
+        items: ["Agence Carburant"]
+      },
+      {
+        title: "Client",
+        items: ["Jam.gg"]
+      },
+      {
+        title: "Annee",
+        items: [2023]
+      },
+    ],
+    content: [
+      {
+        type: "text",
+        content: "Jam.gg est une plateforme de retrogaming permettant de jouer à des classiques du jeu vidéo, directement dans un navigateur web."
+      },
+      {
+        type: "text",
+        content: "En support de l'équipe Carburant, j'ai pris en charge l'intégration du site vitrine de présentation de la plateforme."
+      },
+      {
+        type: "video",
+        mime: "video/mp4",
+        url: "adidas/adidas-scroll.mp4",
+        variant: "full",
+      },
+      {
+        type: "image",
+        url: "adidas/ecodesign.jpg",
+        variant: "container",
+      },
+    ]
   },
   {
     slug: "the-sinking-city",
